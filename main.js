7874e70fe2bc851700d4149bec0b4f51d053537e
Никита <131952241+0NothingElse0@users.noreply.github.com>
2023-05-05 16:55:21 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,15 +4,50 @@ const path = require("path");
 const sourceDir = __dirname + "\\source";//путь к папке с проектом
 const distDir = __dirname + "\\dist";// путь к папке с результатом
 
+//1 вариант миграции
+const firtsVariant = (line) => {
+    let sIndex, eIndex;
+    let res;
+    res = "import";
+    sIndex = line.indexOf(" ");
+    eIndex = line.indexOf(" ", sIndex + 1);
+    res += line.substring(sIndex, eIndex) + " from ";
+    sIndex = line.indexOf("'");
+    eIndex = line.indexOf("'", sIndex + 1);
+    res += line.substring(sIndex, eIndex) + "';";
+    res = res.replace(/\\/g, '');
+    res += "\r";
+    return res;
+};
+
+//2 вариант миграции
+const secondVariant = (line) => {
+    let sIndex, eIndex;
+    let res;
+    res = "import ";
+    sIndex = line.indexOf(" ");
+    eIndex = line.indexOf(" ", sIndex + 1);
+    res += "{" + line.substring(sIndex + 1, eIndex) + "}" + " from ";
+    sIndex = line.indexOf("'");
+    eIndex = line.indexOf("'", sIndex + 1);
+    res += line.substring(sIndex, eIndex) + "';";
+    res = res.replace(/\\/g, '');
+    res += "\r";
+    return res;
+};
+
 // Возвращает данные для файла с заменённым require на import
 const getResultFile = (path) => {
     let dataArray = fs.readFileSync(path).toString().split("\n");
-    for (i in dataArray) {
-        if (dataArray[i].includes("require")) {
-            dataArray[i] = dataArray[i].replace("const", "import");
-            dataArray[i] = dataArray[i].replace("= require", "from");
-            dataArray[i] = dataArray[i].replace("(", " ");
-            dataArray[i] = dataArray[i].replace(")", "");
+    for(i in dataArray) {
+        if(dataArray[i].includes("require")) {
+            if((dataArray[i].startsWith("const") || dataArray[i].startsWith("var")) 
+            && (dataArray[i].endsWith(";") || dataArray[i].endsWith(";\r"))){
+                if(dataArray[i].endsWith(");") || dataArray[i].endsWith(");\r"))
+                    dataArray[i] = firtsVariant(dataArray[i]);
+                else
+                    dataArray[i] = secondVariant(dataArray[i]);  
+            }
         }
     }
     return dataArray;
@@ -25,7 +60,7 @@ const getFilesRecursively = (directory) => {
         let absolutePath = path.join(directory, file);//получаем абсолютный путь к объекту
         let resPath = absolutePath.replace(sourceDir, distDir);//получаем абсолютный путь к результирующему объекту
         //если объект - папка, создаем папку в результирующей директории
-        if (fs.statSync(absolutePath).isDirectory()) {
+        if(fs.statSync(absolutePath).isDirectory()) {
             fs.mkdirSync(resPath, { recursive: true });
             getFilesRecursively(absolutePath);//рекурсивной проверяем эту папку
         } else {
@@ -33,6 +68,7 @@ const getFilesRecursively = (directory) => {
             if(absolutePath.substring(absolutePath.length - 3) == ".js"){
                 //получаем изменённый данные и создаем файл
                 let dataArray = getResultFile(absolutePath);
+                fs.writeFileSync(resPath, "", {encoding: "utf-8"});
                 for(i in dataArray)
                     fs.writeFileSync(resPath, dataArray[i], {encoding: "utf-8", flag: "a"});
             }else{
